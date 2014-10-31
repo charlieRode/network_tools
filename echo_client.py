8296b22189f52a8df1d69fce5f744f56d8d7b2eb
Charlie Rode <chrode10@gmail.com>
2014-10-30 19:02:33 -0700

    Add sufficiently working echo server and client scripts

--- echo_client.py
+++ echo_client.py
@@ -2,12 +2,17 @@
 import socket, sys
 
 message= sys.argv[1]
-
+port= 50000
+address= '127.0.0.1'
 
 client_socket= socket.socket(
     socket.AF_INET,
     socket.SOCK_STREAM,
     socket.IPPROTO_IP)
-client_socket.connect(('127.0.0.1', 50002))
+client_socket.connect((address, port))
 client_socket.sendall(message)
 client_socket.shutdown(socket.SHUT_WR)
+
+msg= client_socket.recv(1024)
+
+print msg
--- echo_server.py
+++ echo_server.py
@@ -6,8 +6,11 @@ server_socket= socket.socket(
     socket.AF_INET,
     socket.SOCK_STREAM,
     socket.IPPROTO_IP)
-server_socket.bind(('127.0.0.1', 50002))
+server_socket.bind(('127.0.0.1', 50000))
 server_socket.listen(1)
 conn, addr= server_socket.accept()
 
-print conn.recv(32)
+msg = conn.recv(1024)
+conn.sendall(msg)
+conn.shutdown(socket.SHUT_WR)
+conn.close()

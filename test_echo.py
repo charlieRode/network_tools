ccebabfc39fbb43c7f0f11dae7b5aa288e565788
Charlie Rode <chrode10@gmail.com>
2014-10-30 19:42:28 -0700

    Add test_connection unit test. This single test seems to cover it.

--- test_echo.py
+++ test_echo.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 import pytest
 import echo_server
-import echo_client
+from threading import Thread
+import socket
 
+
+def dummy_client():
+    message = "Christian Bale is a terrible actor."
+    port = 50000
+    address = '127.0.0.1'
+    client_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    client_socket.connect((address, port))
+    client_socket.sendall(message)
+    client_socket.shutdown(socket.SHUT_WR)
+
+    return client_socket.recv(1024)
+
+
+def test_connection():
+    thread = Thread(target=echo_server.main)
+    thread.start()
+    assert dummy_client() == "Christian Bale is a terrible actor."

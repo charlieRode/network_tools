e2971a79e377526823bdf52313e11ab0c4c02e5e
Charlie Rode <chrode10@gmail.com>
2014-10-30 19:12:46 -0700

    Package echo scripts into callable functions in oder to be able to test

--- echo_client.py
+++ echo_client.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
-import socket, sys
+import socket
+import sys
 
-message= sys.argv[1]
-port= 50000
-address= '127.0.0.1'
 
-client_socket= socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_IP)
-client_socket.connect((address, port))
-client_socket.sendall(message)
-client_socket.shutdown(socket.SHUT_WR)
+def main():
+    message = sys.argv[1]
+    port = 50000
+    address = '127.0.0.1'
+    client_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    client_socket.connect((address, port))
+    client_socket.sendall(message)
+    client_socket.shutdown(socket.SHUT_WR)
 
-msg= client_socket.recv(1024)
+    return client_socket.recv(1024)
 
-print msg
+if __name__ == '__main__':
+    print main()
--- echo_server.py
+++ echo_server.py
@@ -2,15 +2,16 @@
 import socket
 
 
-server_socket= socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_IP)
-server_socket.bind(('127.0.0.1', 50000))
-server_socket.listen(1)
-conn, addr= server_socket.accept()
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
+                                  socket.IPPROTO_IP)
+    server_socket.bind(('127.0.0.1', 50000))
+    server_socket.listen(1)
+    conn, addr = server_socket.accept()
+    msg = conn.recv(1024)
+    conn.sendall(msg)
+    conn.shutdown(socket.SHUT_WR)
+    conn.close()
 
-msg = conn.recv(1024)
-conn.sendall(msg)
-conn.shutdown(socket.SHUT_WR)
-conn.close()
+if __name__ == '__main__':
+    main()

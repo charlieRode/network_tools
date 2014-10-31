5c30173731d058b51d7a94238a3ccf5984e2e790
Charlie Rode <chrode10@gmail.com>
2014-10-30 19:53:20 -0700

    Change format to satify pedantic linter

--- echo_server.py
+++ echo_server.py
@@ -3,8 +3,10 @@ import socket
 
 
 def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
-                                  socket.IPPROTO_IP)
+    server_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
     server_socket.bind(('127.0.0.1', 50000))
     server_socket.listen(1)
     conn, addr = server_socket.accept()
